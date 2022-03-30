9f92ae936adbfcdb0e212d4e940faccfddd5023c
hi-june <98803599+hi-june@users.noreply.github.com>
2022-03-31 00:10:12 +0900

    대규모 편집

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅해보기",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["IT News"],
+        "tags": [],
+        "url": "/it%20news/my-first-post/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기2",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["IT News"],
+        "tags": [],
+        "url": "/it%20news/my-second-post/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기2",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["Algorithm"],
+        "tags": [],
+        "url": "/algorithm/my-third-post/",
+        "teaser": null
+      }]
