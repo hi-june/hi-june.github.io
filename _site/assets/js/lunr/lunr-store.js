c99f48ec25315263a3ecc4ff522d3e8ebd98f115
hi-june <98803599+hi-june@users.noreply.github.com>
2022-03-31 18:15:25 +0900

    테스트용 게시물 업로드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,61 @@
 var store = [{
-        "title": "[Jekyll] 블로그 포스팅해보기",
+        "title": "[Jekyll] 블로그 포스팅해보기1",
         "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["IT News"],
-        "tags": [],
+        "tags": ["Test Tag1"],
         "url": "/it%20news/my-first-post/",
         "teaser": null
       },{
         "title": "[Jekyll] 블로그 포스팅해보기2",
         "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["IT News"],
-        "tags": [],
+        "tags": ["Test tag1","Test Tag2"],
         "url": "/it%20news/my-second-post/",
         "teaser": null
       },{
-        "title": "[Jekyll] 블로그 포스팅해보기2",
+        "title": "[Jekyll] 블로그 포스팅해보기3",
         "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["Algorithm"],
-        "tags": [],
+        "tags": ["Test Tag1","Test Tag3"],
         "url": "/algorithm/my-third-post/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["DataStructure"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/datastructure/test1/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["Programmers"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/programmers/test17/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["Algorithm"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/algorithm/test2/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["BOJ"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/boj/test3/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["CodeUp"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/codeup/test4/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["Data Structure"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/data%20structure/test5/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅해보기1",
+        "excerpt":"1. 포스트 소제목 테스트1  내용도 테스트 해보자..1       2. 포스트 소제목 테스트2  내용도 테스트 해보자..2   3. 포스트 소제목 테스트3  내용도 테스트 해보자..3  ","categories": ["DataBase Concept"],
+        "tags": ["Test Tag1","Test Tag2"],
+        "url": "/database%20concept/test6/",
         "teaser": null
       }]
