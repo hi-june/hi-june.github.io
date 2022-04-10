16d23e9ba2cb104a118aec0c5253b98ae51e5e78
hi-june <98803599+hi-june@users.noreply.github.com>
2022-04-10 14:58:17 +0900

    Android Chap01 Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/spring/Section01/",
         "teaser": null
       },{
+        "title": "[안드로이드 기초] Chapter 01 - 안드로이드의 개요와 개발환경",
+        "excerpt":"1. 안드로이드 개요 안드로이드 주요 기능 안드로이드는 모바일 기기를 위한 운영체제로 안드로이드 SDK는 JAVA 기반으로 안드로이드 애플리케이션을 개발할 수 있는 API를 제공하고 있다. 애플리케이션 프레임워크를 통해서 제공되는 API를 사용함으로써 코드를 재사용하여 효율적이고 빠른 애플리케이션 개발 가능 모바일 기기에 최적화된 달빅 또는 아트런타임(ART : Android Run Time) 제공 2D 그래픽 및...","categories": ["Android Basic"],
+        "tags": ["Kotlin"],
+        "url": "/android%20basic/Chapter01/",
+        "teaser": null
+      },{
         "title": "[스프링 입문] Section 02 - 스프링 웹 개발 기초",
         "excerpt":"Web을 개발하는 3가지 방법 정적 컨텐츠: 파일을 그냥 웹 브라우저에 내려주는 것 MVC와 템플릿 엔진: html, PHP, JSP 같은 파일을 서버에서 프로그래밍해서 동적으로 바꾼 후 내려주는 것 API: 예를 들자면, 만약 Android나 ios같은 Client랑 개발해야할 떄, ‘jason’이라는 포멧으로 내려주는 것 1. 정적 컨텐츠 1st. 웹 브라우저가 요청을 서버에 넘겨줌 2nd....","categories": ["Spring"],
         "tags": ["Java"],
