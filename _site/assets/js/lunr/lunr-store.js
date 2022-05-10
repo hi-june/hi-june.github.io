b428fa60cc8685fee4e3e1bbe3ff59b03e294e0e
hi-june <98803599+hi-june@users.noreply.github.com>
2022-05-10 18:10:11 +0900

    임시저장

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,9 +11,21 @@ var store = [{
         "url": "/android%20basic/Chapter01/",
         "teaser": null
       },{
+        "title": "[안드로이드 기초] Chapter 04 - 기본 위젯 익히기",
+        "excerpt":"1. 뷰의 개요 뷰와 뷰그룹 안드로이드 화면에서 실제로 사용되는 것들은 모두 View라는 클래스(‘위젯’이라고도 함)의 상속 받음 버튼, 라디오버튼, 이미지 등등 모두 View의 서브클래스이다. 레이아웃: 다른 위젯을 담을 수 있는 위젯. ViewGroup이라는 클래스 아래에 존재함. 위젯은 단독 존재하지 않고, 위젯을 담아 배치하는 틀이 레이아웃이다. (위젯을 포함하는 컨테이너 역할을 함) View 클래스의...","categories": ["Android Basic"],
+        "tags": ["Kotlin"],
+        "url": "/android%20basic/Chapter04/",
+        "teaser": null
+      },{
         "title": "[스프링 입문] Section 02 - 스프링 웹 개발 기초",
         "excerpt":"Web을 개발하는 3가지 방법 정적 컨텐츠: 파일을 그냥 웹 브라우저에 내려주는 것 MVC와 템플릿 엔진: html, PHP, JSP 같은 파일을 서버에서 프로그래밍해서 동적으로 바꾼 후 내려주는 것 API: 예를 들자면, 만약 Android나 ios같은 Client랑 개발해야할 떄, ‘jason’이라는 포멧으로 내려주는 것 1. 정적 컨텐츠 1st. 웹 브라우저가 요청을 서버에 넘겨줌 2nd....","categories": ["Spring"],
         "tags": ["Java"],
         "url": "/spring/Section02/",
+        "teaser": null
+      },{
+        "title": "[안드로이드 기초] Chapter 05 - 레이아웃 익히기",
+        "excerpt":"1. 레이아웃의 개요 레이아웃의 기본 개념 레이아웃은 레이아웃 안에 존재하는 위젯을 배치하게 해준다. 다음과 같은 속성들이 있다. orientation: 레이아웃 안에 배치할 위젯의 수직 또는 수평 방향을 설정 gravity: 레이아웃 안에 배치할 위젯의 정렬 방향을 좌측, 우측, 중앙 등으로 설정 padding: 레이아웃 안에 배치할 위젯의 여백을 설정 layout_weight: 레이아웃이 전체 화면에서...","categories": ["Android Basic"],
+        "tags": ["Kotlin"],
+        "url": "/android%20basic/Chapter05/",
         "teaser": null
       }]
