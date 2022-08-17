866215448a11009177b771ed676b1502b61b6924
hi-june <wnsgur735@gmail.com>
2022-08-17 19:46:56 +0900

    Team_ForV 아키텍처 post 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -42,8 +42,14 @@ var store = [{
         "teaser": null
       },{
         "title": "[Team_ForV] 프로젝트 소개",
-        "excerpt":"들어가기에 앞서.. 여름방학을 맞아 학교 사람들과 같이 프로젝트를 진행해보았다. AI 기술을 활용하여 web 서비스를 만들며 end to end 경험을 해보는 경험을 했다. 프로젝트가 끝나고 기억이 흐려지기 전에 프로젝트를 진행하며 겪었던 시행착오들과 공부했던 내용을 차곡차곡 정리해보고자 한다. 프로젝트 소개 프로젝트를 시작하기에 앞서 여러 AI 관련 프로젝트들을 참고하였고, 그 중 눈에 들어왔던...","categories": ["Team_ForV"],
+        "excerpt":"들어가기에 앞서.. 여름방학을 맞아 학교 사람들과 같이 프로젝트를 진행해보았다. AI 기술을 활용한 web 서비스를 end to end로 개발해보는 경험을 했다. 프로젝트가 끝나고 기억이 흐려지기 전에 프로젝트를 진행하며 겪었던 시행착오들과 공부했던 내용을 차곡차곡 정리해보고자 한다. 프로젝트 소개 프로젝트를 시작하기에 앞서 여러 AI 관련 프로젝트들을 참고하였고, 그 중 눈에 들어왔던 기술은.. 네이버에서...","categories": ["Team_ForV"],
         "tags": ["Info"],
         "url": "/team_forv/Team_ForV02/",
+        "teaser": null
+      },{
+        "title": "[Team_ForV] 아키텍처 및 기술스택 소개",
+        "excerpt":"아키텍처 소개 Front-End Back-End DevOps AI (Tools, Env) UI/UX, MockUp Design Team Collaboration Tool 프로젝트의 흐름 이 프로젝트의 기본 흐름은 이러하다. react(클라이언트)로부터 사용자의 요청이 들어오면, nginx(proxy 서버)가 클라이언트와 서버 사이에서 대리로 통신을 수행해준다. django나 flask는 python으로 쓰여진 web application이기 때문에 gunicorn과 같은 wsgi 서버를 통해 forwarding해주게 되고, 사용자의 요청에 따라...","categories": ["Team_ForV"],
+        "tags": ["Info"],
+        "url": "/team_forv/Team_ForV03/",
         "teaser": null
       }]
